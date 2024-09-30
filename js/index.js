21195f8d4f56d9424b3d4c5d3b121718ffecc507
yanetcoronelok <ya.coronel@gmail.com>
2024-09-30 01:28:02 -0300

    Codigo Js para primer preentrega - mejora del codigo

--- js/index.js
+++ js/index.js
@@ -1,63 +1,49 @@
-let clave;
+function login() {
+  let claveIngresada;
+  let intentos = 0;
+  const MAX_INTENTOS = 3;
+  const CLAVE_CORRECTA = "entrega1";
 
-let intentos = 0;
-const MAX_INTENTOS = 3;
-
-do {
-    clave = prompt('Ingresa la clave');
-    intentos ++;
-    
-    if(clave ==='entrega1'){
-        console.log("Contrasenia correcta");
-        break;
+  do {
+    claveIngresada = prompt("Ingresa la clave");
+    intentos++;
+    if (claveIngresada === CLAVE_CORRECTA) {
+      return true;
     }
+  } while (intentos < MAX_INTENTOS);
+  return false;
+}
 
-    if(intentos >= MAX_INTENTOS){
-        console.log("Se alcanzo la cantidad maxima de intentos");
-        break;
-    }
+function calcularTotal(cantidad, formaDePago) {
+  const precioPorProducto = 100;
+	let total = precioPorProducto * cantidad;
 
+  // Calcular descuentos y recargos según las condiciones
 
-} while(true);
+  if (cantidad >= 5) {
+	total -=  cantidad * precioPorProducto * 0.05;
+  } 
+  if (formaDePago === 1) {
+    total -= cantidad * precioPorProducto * 0.1;
+  } else if (formaDePago === 3) {
+    total += cantidad * precioPorProducto * 0.08;
+  }
 
+  return total;
+}
+// Inicio del programa
+let loginExitoso = login();
 
+if (loginExitoso) {
+  const cantidadProductosSeleccionados = parseInt(
+    prompt("Cantidad de productos seleccionados: ")
+  );
+  const formaDePago = parseInt(
+    prompt("Ingrese forma de pago: 1-Efectivo, 2-Debito, 3-Credito")
+  );
 
-let cantidad;
-let formaDePago;
-let tasaDescuento = 0;
-let tasaRecargo = 0;
-let precioPorProducto = 100; 
-
-
-
-function calcularTotal (cantidad, formaDePago){
-
-    descuento = 0;
-    recargo = 0; 
-
-
-     // Calcular descuentos y recargos según las condiciones
-
-    if ( cantidad >= 5){
-        descuento = cantidad * precioPorProducto * 0.05;
-    } else if (formaDePago === 1){
-        descuento = cantidad * precioPorProducto * 0.1;
-    } else if (formaDePago === 3){
-        recargo = cantidad * precioPorProducto * 0.05;
-    } 
-
-
- // Calcular el total
- let total = (cantidad * precioPorProducto) - descuento + recargo; 
-
-return total;
-
-
-} 
-
-cantidadProductosSeleccionados = parseInt(prompt("Cantidad de productos seleccionados: ")); 
-formaDePago = parseInt(prompt("Ingrese forma de pago: 1-Efectivo, 2-Debito, 3-Credito"))
-
-
-let totalVenta = calcularTotal(cantidadProductosSeleccionados, formaDePago);
-console.log("El total de la venta es: $" + totalVenta);
+  let totalVenta = calcularTotal(cantidadProductosSeleccionados, formaDePago);
+  console.log("El total de la venta es: $" + totalVenta);
+} else {
+  alert("No se pudo loguear");
+}
